16da9582312aedc7ed0afd211a6181f420c22a64
MirzaAbazovic <mirza.abazovic@gmail.com>
2015-08-08 01:56:08 +0200

    Tel i sms kontakta rade

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,50 @@
 angular.module('starter.controllers', [])
 
-.controller('ContactCtrl', function($scope) {
+.controller('ContactCtrl', function($scope,$cordovaSms,$cordovaDialogs,$ionicPlatform) {
+  //var mob =
   $scope.contact = {
-    "mob":+38761123123,
-    "mobDisplay":"(+387) 61 123 123",
+    "mob":+38761676178,
+    "mobDisplay":"(+387) 61 676 178",
     "email":"hracic@sarajevoosiguranje.ba"
   }
+  
+$scope.sendSms = function(){
+
+$ionicPlatform.ready(function() {
+  $cordovaDialogs.prompt('Poruka', 'Upisite poruku', ['Odustani','Pošalji'], '')
+    .then(function(result) {
+      var input = result.input1;
+      // no button = 0, 'OK' = 1, 'Cancel' = 2
+      var btnIndex = result.buttonIndex;
+      if(btnIndex===1 && input.length>0){
+                var options = {
+                replaceLineBreaks: false, // true to replace \n by a new line, false by default
+                android: {
+                //intent: 'INTENT'  // send SMS with the native android SMS messaging
+                intent: '' // send SMS without open any other app
+                }}
+      $cordovaSms.send('061636785', input, options)
+      .then(function() {
+        // Success! SMS was sent
+         $cordovaDialogs.alert('Poruka je poslana')
+      }, function(error) {
+        // An error occurred
+          $cordovaDialogs.alert('Došlo je do greške: Poruka NIJE poslana')
+    
+      }); 
+      //if end
+    }
+    //msg dialog then
+    });
+  //ionicPlatformReady
+  });
+//smsSend END
+}
+//controller
 })
 
-.controller('PolicyCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+.controller('PolicyCtrl', function($scope) {
+  
 })
 
 .controller('PolicyPzoCtrl', function($scope) {
@@ -32,5 +56,5 @@ angular.module('starter.controllers', [])
 })
 
 .controller('MapCtrl', function($scope) {
- 
+ $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
 });
