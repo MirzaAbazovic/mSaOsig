39e45c43475b3e67a30c63b59ad672e22e85c4de
MirzaAbazovic <mirza.abazovic@gmail.com>
2015-08-09 18:12:22 +0200

    Obojeno u crveno - boja firme

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,7 @@ angular.module('starter.controllers', [])
 .controller('ContactCtrl', function($scope,$cordovaSms,$cordovaDialogs,$ionicPlatform,$cordovaEmailComposer) {
   //var mob =
   $scope.contact = {
-    "mob":+38761676178,
+    "mob":"+38761676178",
     "mobDisplay":"(+387) 61 676 178",
     "email":"hracic@sarajevoosiguranje.ba"
   }
@@ -20,10 +20,10 @@ $ionicPlatform.ready(function() {
                 var options = {
                 replaceLineBreaks: false, // true to replace \n by a new line, false by default
                 android: {
-                //intent: 'INTENT'  // send SMS with the native android SMS messaging
-                intent: '' // send SMS without open any other app
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
                 }}
-      $cordovaSms.send('061636785', input, options)
+      $cordovaSms.send($scope.contact.mob, input, options)
       .then(function() {
         // Success! SMS was sent
          $cordovaDialogs.alert('Poruka je poslana')
@@ -43,39 +43,184 @@ $ionicPlatform.ready(function() {
 
 
 
-
-
-
 $scope.sendEmail = function() {
 $ionicPlatform.ready(function() {
-
             $cordovaEmailComposer.open({
-                to:          ["mirza.abazovic@gmail.com"], // email addresses for TO field
-                isHtml:    false, // indicats if the body is HTML or plain text
+                to: $scope.contact.email, // email addresses for TO field
+                isHtml: true, // indicats if the body is HTML or plain text
             }, function () {
                 console.log('email view dismissed');
             },
             this);    
-        
 })
+//sendEmail END
 }
-//controller
+
+//controller END
 })
 
 .controller('PolicyCtrl', function($scope) {
-  
 })
 
 .controller('PolicyPzoCtrl', function($scope) {
- 
 })
 
 .controller('PolicyAoCtrl', function($scope) {
-  
 })
-.controller('MapCtrl', function($scope, $ionicLoading, $compile,$cordovaGeolocation) {
-  $scope.initialize = function() {
 
+
+
+
+.controller('MapCtrl', function($scope, $ionicLoading, Geo) {
+  console.log("MapCtrl");
+
+  $scope.initialise = function () {
+    console.log("In Google.maps.event.addDomListener");
+    var myLatlng = new google.maps.LatLng(43.855224, 18.420757);
+    var mapOptions = {
+      center: myLatlng,
+                  zoom: 13,
+                  mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+       
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    //Markeri firme 
+    var image = 'img/marker.jpg';
+    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    //Direkcija marker
+    var contentDirekcija = '<div>'+
+      '<b>Direkcija</b>'+
+      '<p>Adresa: <b>Čobanija 14</b> <p>'+
+      '<div>'+
+      '<p>Tel: <b><a href="tel:+38733203270">(033)203-270</a></b>; <b><a href="tel:+38733443581">(033)443-581</a></b></p>'+
+      '<p>Radno vrijeme</p>'+
+      '<p>Prodaja polica: <b>07:30 - 15:30</b></p>'
+      '</div>'+
+      '</div>';
+      var infowindowDirekcija = new google.maps.InfoWindow({content: contentDirekcija});
+      var markerDirekcija = new google.maps.Marker({
+      position: new google.maps.LatLng(43.855050, 18.420705),
+      map: map,
+      title: 'Direkcija | Čobanija 14 | (033)203-270 (033)443-581',
+      icon: image
+    });
+    google.maps.event.addListener(markerDirekcija, 'click', function() {
+      infowindowDirekcija.open(map,markerDirekcija);
+    });
+    //Podruznica Sarajevo Marker 
+      var contentPodruznicaSarajevo = '<div>'+
+       '<b>Podruznica Sarajevo</b>'+
+      '<p>Adresa: <b>Maršala Tita 29</b> <p>'+
+      '<div>'+
+      '<p>Tel: <b><a href="tel:+38733569600">(033)569-600</a></b>; <b><a href="tel:+38733223835">(033)223-835</a></b></p>'+
+      '<p>Radno vrijeme</p>'+
+      '<p>Prodaja polica: <b>07:30 - 17:30</b></p>'+
+      '<p>Prijava šteta: <b>07:30 - 15:30</b></p>'+
+      '</div>'+
+      '</div>';
+      var infowindowPodruznicaSarajevo = new google.maps.InfoWindow({content: contentPodruznicaSarajevo});
+      var markerPodruznicaSarajevo = new google.maps.Marker({
+      position: new google.maps.LatLng(43.857547, 18.420854),
+      map: map,
+      title: 'Podružnica Sarajevo | Maršala Tita 29 | Tel: (033)569-600  (033)223-835',
+      icon: image
+    });
+    google.maps.event.addListener(markerPodruznicaSarajevo, 'click', function() {
+    infowindowPodruznicaSarajevo.open(map,markerPodruznicaSarajevo);
+  });
+    //Marker Podruznica Tuzla
+    var markerFiliTuzla = new google.maps.Marker({
+      position: new google.maps.LatLng(44.540692, 18.665643),
+      map: map,
+      title: 'Podružnica Tuzla | Slatina 4 | (035)369-100 (035)369-101',
+      icon: image
+    });
+
+    var markerTehnickiVogosca = new google.maps.Marker({
+      position: new google.maps.LatLng(43.896779, 18.361302),
+      map: map,
+      title: 'Tehnički pregled Vogošča | Muje Šejte 7 | (033)426-576',
+       icon: image
+    });
+    //
+
+    Geo.getLocation().then(function(position) {
+      console.log(position);
+      map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      var myLocation = new google.maps.Marker({
+        position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+        map: map,
+        title: "Moja lokacija"
+      });
+    });
+
+         
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      console.log(pos);
+      map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      var myLocation = new google.maps.Marker({
+        position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+        map: map,
+        title: "Moja lokacija"
+      });
+    }, function() { console.log("Cannot find location");});
+
+    $scope.map = map;
+  };
+  google.maps.event.addDomListener(document.getElementById("map"), 'load', $scope.initialise());
+         
+});
+
+
+/*
+.controller('MapCtrl', function($scope, $ionicLoading, Geo) {
+  console.log("MapCtrl");
+  $scope.initialise = function () {
+    console.log("In Google.maps.event.addDomListener");
+    var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+    var mapOptions = {
+      center: myLatlng,
+                  zoom: 16,
+                  mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+       
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    Geo.getLocation().then(function(position) {
+      console.log(position);
+      map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      var myLocation = new google.maps.Marker({
+        position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+        map: map,
+        title: "My Location"
+      });
+    });
+
+         
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      console.log(pos);
+      map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      var myLocation = new google.maps.Marker({
+        position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+        map: map,
+        title: "My Location"
+      });
+    }, function() { console.log("Cannot find location");});
+
+    $scope.map = map;
+  };
+  google.maps.event.addDomListener(document.getElementById("map"), 'load', $scope.initialise());
+         
+});
+*/
+
+
+
+
+/*
+.controller('MapCtrl', function($scope, $ionicLoading, $compile,$cordovaGeolocation,$ionicPlatform) {
+  $ionicPlatform.ready(function() {    
+  $scope.initialize = function() {
   var posOptions = {timeout: 10000, enableHighAccuracy: false};
   $cordovaGeolocation
     .getCurrentPosition(posOptions)
@@ -137,15 +282,13 @@ $ionicPlatform.ready(function() {
 
       
       $scope.map = map;
-
-
 //
-    }, function(err) {
+    }, 
+    function(err) {
       // error
     });
-
-
-  
   }
   //google.maps.event.addDomListener(window, 'load', initialize);
 })
+})
+*/
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,63 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
-
+/**
+ * A simple example service that returns some data.
+ */
+.factory('Geo', function($q) {
   return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    reverseGeocode: function(lat, lng) {
+    console.log("GEO FACTORY");
+      var q = $q.defer();
+      var geocoder = new google.maps.Geocoder();
+      console.log("geocoder");
+      geocoder.geocode({
+        'latLng': new google.maps.LatLng(lat, lng)
+      }, function(results, status) {
+         console.log("geocoder.geocode");
+        if (status == google.maps.GeocoderStatus.OK) {
+          console.log('Reverse', results);
+          if(results.length > 1) {
+            var r = results[1];
+            var a, types;
+            var parts = [];
+            var foundLocality = false;
+            var foundState = false;
+            for(var i = 0; i < r.address_components.length; i++) {
+              a = r.address_components[i];
+              types = a.types;
+              for(var j = 0; j < types.length; j++) {
+                if(!foundLocality && types[j] == 'locality') {
+                  foundLocality = true;
+                  parts.push(a.long_name);
+                } else if(!foundState && types[j] == 'administrative_area_level_1') {
+                  foundState = true;
+                  parts.push(a.short_name);
+                }
+              }
+            }
+            console.log('Reverse', parts);
+            q.resolve(parts.join(', '));
+          }
+        } else {
+          console.log('reverse fail', results, status);
+          q.reject(results);
         }
-      }
-      return null;
+      })
+
+      return q.promise;
+    },
+    getLocation: function() {
+      console.log("Geo.getLocation");
+      var q = $q.defer(); 
+      console.log("Calling Navigator.geolocation");
+      navigator.geolocation.getCurrentPosition(function(position) {
+        q.resolve(position);
+  console.log(position);
+      }, function(error) {
+        q.reject(error);
+      });
+
+      return q.promise;
     }
   };
-});
+})
